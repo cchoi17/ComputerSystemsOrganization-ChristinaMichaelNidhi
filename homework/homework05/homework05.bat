6b64d33f6e39b9fbcbd9f9a1e83cc14e5347165b
B.J. Johnson <bjohnson05@gmail.com>
2022-11-10 22:55:44 -0800

    added homework05 evaluation

---
+++ homework/homework05/homework05.bat
@@ -0,0 +1,44 @@
+del testresults.txt
+gcc dec2bin.c      -o dec2bin.exe       >> testresults.txt
+gcc dec2hex.c      -o dec2hex.exe       >> testresults.txt
+gcc holdit.c       -o holdit.exe        >> testresults.txt
+gcc timesTables.c  -o timesTables.exe   >> testresults.txt
+gcc wordcount.c    -o wordcount.exe     >> testresults.txt
+echo . >> testresults.txt
+echo "running tests on dec2bin" >> testresults.txt
+dec2bin >> testresults.txt
+dec2bin 5 >> testresults.txt
+dec2bin 123456 >> testresults.txt
+dec2bin 65535 >> testresults.txt
+dec2bin 977272832 >> testresults.txt
+dec2bin 1073746484 >> testresults.txt
+echo . >> testresults.txt
+echo "running tests on dec2hex" >> testresults.txt
+dec2hex >> testresults.txt
+dec2hex 5 >> testresults.txt
+dec2hex 123456 32 >> testresults.txt
+dec2hex 65535 >> testresults.txt
+dec2hex 977272832 32 >> testresults.txt
+dec2hex 1073746484 >> testresults.txt
+dec2hex 5 64 >> testresults.txt
+dec2hex 123456 64 >> testresults.txt
+dec2hex 65535 63 >> testresults.txt
+dec2hex 977272832 64 >> testresults.txt
+dec2hex 1073746484 >> testresults.txt
+echo . >> testresults.txt
+echo "running tests on timesTables" >> testresults.txt
+timesTables >> testresults.txt
+timesTables 2 >> testresults.txt
+timesTables 5 >> testresults.txt
+timesTables 10 >> testresults.txt
+timesTables 12 >> testresults.txt
+timesTables 13 >> testresults.txt
+echo . >> testresults.txt
+echo "running test on holdit" >> testresults.txt
+holdit >> testresults.txt
+echo . >> testresults.txt
+echo "running test on wordcount" >> testresults.txt
+wordcount >> testresults.txt
+wordcount inputfile.txt >> testresults.txt
+
+
